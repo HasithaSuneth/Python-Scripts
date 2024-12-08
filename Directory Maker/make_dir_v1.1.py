7c5652e5c0a0de3183ef4d1656f460b9ca7c2205
Hasitha Suneth <87106402+HasithaSuneth@users.noreply.github.com>
2024-12-08 15:19:14 +0530

    Update: make dir

--- Directory Maker/make_dir_v1.1.py
+++ Directory Maker/make_dir_v1.1.py
@@ -7,17 +7,21 @@ name_list = ["The.War.with.Grandpa.2020.480p.BluRay.x264.400MB-Pahe.in", "Bill.T
 
 name_file = open("other/New_Movies.txt", "r")
 
-
+          
 def from_file(newpath, name_file):
-    name_list = []
-    for i in name_file:
-        name_list.append(i.strip())
+    # Read each line in the file and strip any leading or trailing whitespace
+    name_list = [i.strip() for i in name_file]
+    
+    # Loop through each movie name in the list
     for i in name_list:
+        # Check if the directory corresponding to this movie already exists
         if not os.path.exists(newpath + "\\" + i):
+            # If it doesn't exist, create it
             os.makedirs(newpath + "\\" + i)
 
 
 def from_list(newpath, name_list):
+    # Loop through each movie name in the list
     for i in name_list:
         if not os.path.exists(newpath + "\\" + i):
             os.makedirs(newpath + "\\" + i)
